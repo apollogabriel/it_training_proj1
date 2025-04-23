fcf3805f89b4282a894b6c3289ecc4f352934eba
gab <a.delmundo@psa.gov.ph>
2025-04-23 13:23:20 +0800

    4-23-25 office

---
+++ src/views/AddForm.vue
@@ -0,0 +1,37 @@
+<template>
+
+</template>
+<script>
+    export default{
+        handleSubmit() {
+
+        console.log(this.title, this.details)
+        // console: Test Title Lorem Ipsum dolor sit.
+
+        let project = {
+            title: this.title,
+            details: this.details,
+            completed: false,
+        }
+
+        console.log(project)
+        // console: {title: 'Test Title', details: 'Lorem Ipsum dolor sit.', completed: false}
+
+        fetch('http://localhost:3000/projects', {
+            method: 'POST',
+            header: { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'      
+            },
+            body: JSON.stringify(project)
+        })
+        //.then(() => this.$router.push('/'))
+        .catch(err => console.log(err))
+
+        }
+    }
+
+</script>
+<style>
+
+</style>
